00bd139e4c2d9d7171c024fc2ca6069b1c669d3b
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-06 00:08:21 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,13 +1,38 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  name = ""
+  data[season].each {|item|
+    if item["status"] == "Winner"
+      name = item["name"].split(" ")[0]
+    end
+  }
+  name
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  name = ""
+  data.each {|season, contestants|
+    contestants.each {|contestant|
+      if contestant["occupation"] == occupation
+        name = contestant["name"]
+      end
+    }
+  }
+  name
 end
 
 def count_contestants_by_hometown(data, hometown)
   # code here
+  count = 0
+  data.each {|season, contestants|
+    contestants.each {|contestant|
+      if contestant["hometown"] == hometown
+        count += 1
+      end
+    }
+  }
+  count
 end
 
 def get_occupation(data, hometown)
