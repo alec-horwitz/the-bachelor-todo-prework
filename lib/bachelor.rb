d1293fffdb03bfecde572f2a3e19554939e6f8c4
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-06 00:37:47 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -55,12 +55,9 @@ def get_average_age_for_season(data, season)
   # code here
   n = 0
   d = 0
-  data.each {|season, contestants|
-    contestants.each {|contestant|
-      n += contestant["age"].to_i
-      d += 1
-      binding.pry
-    }
+  data[season].each {|contestants|
+    n += contestant["age"].to_i
+    d += 1
   }
   n/d
 end
