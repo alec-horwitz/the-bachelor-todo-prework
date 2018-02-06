b8a35b542ac9c3a4fa66ba52e78c7dc8ae495cd3
Alec Horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-06 00:19:28 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -56,7 +56,7 @@ def get_average_age_for_season(data, season)
   d = 0
   data.each {|season, contestants|
     contestants.each {|contestant|
-      n += contestant["age"]
+      n += contestant["age"].to_i
       d += 1
     }
   }
